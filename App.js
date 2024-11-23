65d92bd8aa06a2088b475612c85199e7c9b2260e
Karan Dixit <dixitk941@gmail.com>
2024-11-23 10:32:04 +0530

    day1

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WebView 
+        source={{ uri: 'https://sttrika.com' }} 
+        style={{ flex: 1 }} 
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
